b1b8d2348dac1a28219df584820cfad10197cfe7
Martha <perpignan2000@protonmail.com>
2024-07-17 22:50:35 +0200

    test

--- main.py
+++ main.py
@@ -1,15 +1,20 @@
 
+tours = 5 #input
+
 nb = 1
 test=1
 for element in range(0,10):
     element+=1
+    print("")
     nb += 1
     for maria in range(0,nb):
         
-        print("test\n", end='')
+        print("test",end="")
+
+        
         
 
-print()
+
 
 
 
