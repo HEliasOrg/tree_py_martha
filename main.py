f5cb3ee89e56e135780e7285f8b915ef159f3995
Martha <perpignan2000@protonmail.com>
2024-07-18 10:05:32 +0200

    test

--- main.py
+++ main.py
@@ -1,15 +1,15 @@
 
-tours = 5 #input
+tour=int(input("le numero est:")) 
 
 nb = 1
 test=1
-for element in range(0,10):
+for element in range(1,tour+1):
     element+=1
     print("")
     nb += 1
-    for maria in range(0,nb):
-        
+    for maria in range(0,nb+1):
         print("test",end="")
+        
 
         
         
